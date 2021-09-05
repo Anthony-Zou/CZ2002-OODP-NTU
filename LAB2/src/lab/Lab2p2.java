df34174056590f890dba9fc3174a2e68374efefd
Anthony Zou <zouzeren@gmail.com>
2021-09-05 19:52:52 +0800

    Create Lab2p2.java
    A method that is to test students ability to do multiplication. The method will ask a student 5
    multiplication questions one by one and checks the answers.

---
+++ LAB2/src/lab/Lab2p2.java
@@ -0,0 +1,26 @@
+package lab;
+import java.util.Scanner;
+import java.math.*;
+public class Lab2p2 {
+    public static int NumofCorrectAns=0;
+    public static void mulTest()
+    {
+        int a =(int)(Math.random() * 10) + 1;
+        int b =(int)(Math.random() * 10) + 1;
+        int c = a * b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How much is "+a+ " times "+b+"?");
+        int answer = sc.nextInt();
+        if(answer==c)
+        {
+             NumofCorrectAns++;
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+        for(int i=0;i<5;i++){
+        mulTest();}
+        System.out.println(NumofCorrectAns+" answers out of 5 are correct");
+    }
+}
