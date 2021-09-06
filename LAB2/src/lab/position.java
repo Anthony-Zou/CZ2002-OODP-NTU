2d379099ef2095bfc4f117026e2ec72fbcff89d8
Anthony Zou <zouzeren@gmail.com>
2021-09-06 08:36:40 +0800

    Create position.java
    Write the method position() which returns the position of the first appearance of a specified digit in a
    positive number n. The position of the digit is counted from the right and starts from 1. If the required
    digit is not in the number, the method should return -1. For example, position(12315, 1) returns 2 and
    position(12, 3) returns -1

---
+++ LAB2/src/lab/position.java
@@ -0,0 +1,36 @@
+package lab;
+
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class position {
+    public static int position(int n, int digit)
+    {
+        // Converting the integer input to string data
+        String string_number = Integer.toString(n);
+        String digits = Integer.toString(digit);
+        int count=0;
+        // Traversing through the string using for loop
+        for (int i = string_number.length()-1; i >= 0; i--) {
+            count++;
+            // Printing the characters at each position
+            //System.out.println(string_number.charAt(i));
+            if(string_number.charAt(i)==digits.charAt(0))
+            {
+                return count;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Numbers n");
+        int n = sc.nextInt();
+        System.out.println("Enter Digit");
+        int digit = sc.nextInt();
+        System.out.println("position = " + position(n,digit) + "");
+    }
+}
