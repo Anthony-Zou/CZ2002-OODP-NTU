4908f000d2ee31aa6acb675502f479dd0a826150
Anthony Zou <zouzeren@gmail.com>
2021-10-13 00:34:05 +0800

    1

--- LAB4/src/Part1/Strings.java
+++ LAB4/src/Part1/Strings.java
@@ -17,7 +17,8 @@ public static void main (String[] args)
     System.out.println ("\nEnter the strings...");
     for (int i = 0; i < size; i++)
         strList[i] = scan.nextLine();
-    Sorting.selectionSort(strList);
+    //Sorting.selectionSort(strList);
+    Sorting.insertionSort(strList);
     System.out.println ("\nYour strings in sorted order...");
     for (int i = 0; i < size; i++)
         System.out.print(strList[i] + " ");
