e5bb84deef2ef140a1a4ddaef40a212f9ea5f551
Anthony Zou <zouzeren@gmail.com>
2021-10-13 21:23:57 +0800

    1

--- GroupAssignment/src/group6/group6/Main.java
+++ GroupAssignment/src/group6/group6/Main.java
@@ -3,6 +3,10 @@ public class Main {
     public static void main(String[] Args)
     {
         System.out.println("Hello World");
-        System.out.println("Hi, Fang");
+        System.out.println("Hello Team 6");
+        System.out.println("Hello im jy");
+
+        System.out.println("Hello test121233");
+
     }
 }
