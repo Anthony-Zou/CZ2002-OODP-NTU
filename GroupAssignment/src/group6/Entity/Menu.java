b7fce31dc4b082455b4fd75bcfa3e67d3c2b858d
Anthony Zou <zouzeren@gmail.com>
2021-10-14 16:23:06 +0800

    Setting protities of Menu

--- GroupAssignment/src/group6/Entity/Menu.java
+++ GroupAssignment/src/group6/Entity/Menu.java
@@ -1,4 +1,61 @@
 package Entity;
 
+import java.util.Objects;
+
 public class Menu {
+    String itemName;
+    double price;
+    int quantity;
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Override
+    public String toString() {
+        return quantity + "  " + itemName + "  " + price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Menu menu = (Menu) o;
+        return Double.compare(menu.price, price) == 0 && quantity == menu.quantity && Objects.equals(itemName, menu.itemName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(itemName, price, quantity);
+    }
+
+
+    public Menu(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void upDateMenu(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public void RemoveMenu(String itemName, double price, int quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
 }
