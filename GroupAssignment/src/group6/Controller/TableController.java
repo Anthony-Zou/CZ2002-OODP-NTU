cc9ef64ebbffd2d842bbad2d2967957baebd24b7
subeen832715 <62631463+subeen832715@users.noreply.github.com>
2021-11-05 14:58:18 +0800

    merging table controller

--- GroupAssignment/src/group6/Controller/TableController.java
+++ GroupAssignment/src/group6/Controller/TableController.java
@@ -42,38 +42,37 @@ public class TableController {
         }
     }
 
+
+
     public void addTable() {
-        System.out.println("< Add New Table >\n\n");
-        
-        // Input Exception Handling
-        int TableNumber = 0;
+        System.out.println("Add a Table");
+        System.out.println("---------------------");
+        //System.out.println("Enter the Number of the Table:");
+        int TableNumber = 1;
+        ArrayList<Table> presentTables = Database_Controller.readTableList();
+        if (presentTables != null) {
+            TableNumber = presentTables.size() + 1;
+        }
+
+        int capacity = 0;
+
         do {
         	try {
-                System.out.println("Enter the Table Number: ");
-                TableNumber = sc.nextInt();
+                System.out.println("Capacity of Table:");
+                capacity = sc.nextInt();
         	} catch (InputMismatchException e) {
-        		System.out.println("Please enter a valid Table Number!");
+        		System.out.println("Please enter a valid capacity!");
         		System.out.println("\n-----------------------------------\n");
         	}
         	sc.nextLine(); // clears the buffer
-        } while (TableNumber == 0);
-        
-        
-        
-        if (Database_Controller.getTableById(TableNumber) != null) {
-            System.out.println("Table already exists!");
-            return;
-        } else {
-            System.out.println("Capacity of Table: ");
-            int capacity = sc.nextInt();
+        } while (capacity % 2 == 1 || capacity > 10 || capacity < 2);
 
-            Table newtable = new Table(TableNumber, capacity, false);
-            Database_Controller.addTable(newtable);
 
-        }
+        Table newtable = new Table(TableNumber, capacity, false);
+        Database_Controller.addTable(newtable);
+
         printTableDetails();
     }
-
     
     public void deleteTable() {
         System.out.println("< Remove a Table >\n\n");
@@ -111,23 +110,19 @@ public class TableController {
             System.out.println("Table does not exist!");
         } else {
             Table table = Database_Controller.getTableById(TableNumber);
-            if(table.isReserved()==false){
-            table.setReserved(true);}
-            else if(table.isReserved()==true){ table.setReserved(false);}
+            if(!table.isReserved()){
+                table.setReserved(true);}
+            else if(table.isReserved()){ table.setReserved(false);}
             Database_Controller.updateTable(table);
         }
     }
 
     public static void main(String[] args) {
-       TableController t = new TableController();
-//        t.addTable();
-        t.deleteTable();
-//        t.printTableDetails();
-//        t.printAvailableTables(1);
-//        t.updateTable(1);
-//        t.printTableDetails();
-
-
+        TableController Promotion = new TableController();
+        //   Promotion.addPromotion(); Promotion.addPromotion(); Promotion.addPromotion(); Promotion.addPromotion(); Promotion.addPromotion();
+        //Promotion.UpdatePromotion();
+        //Promotion.DeletePromotion();
+        Promotion.printTableDetails();
+        //Promotion.printPromotionById(1);
     }
-
 }
