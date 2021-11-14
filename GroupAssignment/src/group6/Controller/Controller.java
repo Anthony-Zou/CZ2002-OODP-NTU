1bdd34001fe535120821b3a6dadf0c84a09c1c04
Anthony Zou <zouzeren@gmail.com>
2021-11-14 21:27:27 +0800

    DONE !

--- GroupAssignment/src/group6/Controller/Controller.java
+++ GroupAssignment/src/group6/Controller/Controller.java
@@ -1,5 +1,11 @@
 package Controller;
 
+/**
+ Interface Controller for add delete and print
+ @author Zou Zeren
+ @version 1.0
+ @since 24-22-2021
+ */
 public interface Controller {
     public void add();
     public void delete();
